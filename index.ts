d14477257bc4fd6dea9901eecea7b38e02bca6a5
elloit chan <1141151662@qq.com>
2025-03-02 23:05:06 +0800

    feat: 步骤2

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import { readFile } from "fs/promises";
 import { htmlGenerator } from "./generator";
+
 const app = express();
 
 app.get("/:route(*)", async (req, res) => {
+  // 匹配 client.js
+
   if (req.url === "/favicon.ico") {
     return null;
   }
 
   const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (url.pathname === "/client.js") {
+    const content = await readFile("./client.js", "utf8");
+    res.setHeader("Content-Type", "text/javascript");
+    res.end(content);
+    return;
+  }
   const html = await htmlGenerator(url);
   res.setHeader("Content-Type", "text/html");
   res.end(html);
